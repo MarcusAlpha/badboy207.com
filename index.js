b3a3c5f7374afcaa013863b52418e87357128469
MarcusRotherforth <marcusrotherforth@gmail.com>
2020-12-20 19:31:58 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Hello world');
+
+let roblox = 'Hello World!!';
+console.log(roblox);
+
+
+
+let firstName = 'Marcus';
+let secondName = 'Alpha';
+
+
+console.log(firstName, secondName);
+
+let name = 'Marcus'
+let age = 16;
+let isApprooveed = false;
+let firstName2 = undefined;
+let selectedColor = null;
